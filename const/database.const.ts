cbb11abfa3c794515cab6101babf9b5ff3e074ac
valooozz <valentin.corazza@ikmail.com>
2025-10-16 16:18:50 +0200

    fix bug migration bdd a la premiere ouverture de l'app

---
+++ const/database.const.ts
@@ -0,0 +1,2 @@
+export const DB_NAME = 'flipo.db';
+export const LATEST_DB_VERSION = 3;
--- utils/database/migration/getUserVersion.utils.ts
+++ utils/database/migration/getUserVersion.utils.ts
@@ -1,9 +1,11 @@
 import { SQLiteDatabase } from "expo-sqlite";
+import { LATEST_DB_VERSION } from "../../../const/database.const";
 
 export const getUserVersion = async (database: SQLiteDatabase): Promise<number> => {
     try {
         const rows = await database.getAllAsync<{ user_version: number }>('PRAGMA user_version;');
-        return rows?.[0]?.user_version ?? 0;
+        const userVersion = rows?.[0]?.user_version;
+        return userVersion === 0 ? LATEST_DB_VERSION : userVersion;
     } catch (error) {
         console.error('getUserVersion:', error);
         return 0;
